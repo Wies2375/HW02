af95c8b11f3a299c4d25e190190c3b941496d08c
維羽 <wies2375@gmail.com>
2017-03-16 15:20:22 +0800

    no message

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+var i
 for (var i=0; i<5; i=i+1){
     
 d3.select("#first")
   .append("line")
   .attr({
-        "x1"= 0, 
-        "y1"= 45,
-        "x2"= 350,
-        "y2"= 45,
-        "stroke"= "black",
-        "stroke-width"= 1.5>
+    "x1": 0, 
+    "y1": 45+ 27*i,
+    "x2": 350,
+    "y2": 45+ 27*i,
+    "stroke"= "black",
+    "stroke-width"= 1.5>
 });
 }
