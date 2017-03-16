f6ca825100ea8afe015182c9d726acbf13b3576a
維羽 <wies2375@gmail.com>
2017-03-16 16:36:08 +0800

    no message

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-for (var i ;i < 5;i = i + 1){
-    
-d3.select("svg")
-  .append("line")
-  .attr({
-    "x1": 0, 
-    "y1": 45+ 27*i,
-    "x2": 350,
-    "y2": 45+ 27*i,
-    "stroke"= "black",
-    "stroke-width"= 1.5>
-});
+
+for(var i=0; i<5; i=i+1){
+  var num = random(20,300);
+  d3.select("svg")
+    .append("line")
+                  .attr({
+                  "x1":0,
+                  "y1":75+ 30*(i-1),
+                  "x2":300,
+                  "y2":75+ 30*(i-1),
+                  "stroke":"black",
+                  "stroke-width":1.5
+                 }); 
+  
 }
